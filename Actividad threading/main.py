ad113c328b4aee624a441d9fe0a8ce80b0d22fd0
Matias Duhalde <matias.duhalde@uc.cl>
2020-04-16 19:21:16 -0400

    Rename lideres file

--- Actividad threading/main.py
+++ Actividad threading/main.py
@@ -2,15 +2,15 @@ import os
 import sys
 from parametros import bomba_nuclear
 from cargar_tweets import cargar_tweets
-from twitter import Lider_mundial
+from lideres import LiderMundial
 from DoomsdayClock import Doomsday_Clock
 
 class Simulacion:
 
     def __init__(self, tweets_pinto, tweets_trumpzini):
         self.doomsday_clock = Doomsday_Clock(0.25, 51)
-        self.dr_pinto = Lider_mundial('Dr. Pin Tong-Un', tweets_pinto, 0.25, self.doomsday_clock)
-        self.trumpzini = Lider_mundial('Trumpzini',tweets_trumpzini, 0.25, self.doomsday_clock)
+        self.dr_pinto = LiderMundial('Dr. Pin Tong-Un', tweets_pinto, 0.25, self.doomsday_clock)
+        self.trumpzini = LiderMundial('Trumpzini',tweets_trumpzini, 0.25, self.doomsday_clock)
 
     def comenzar(self):
         self.doomsday_clock.start()
@@ -19,6 +19,7 @@ class Simulacion:
         self.doomsday_clock.join()
         print(bomba_nuclear)
         sys.exit()
+
 
 if __name__ == "__main__":
     tweets_pinto = cargar_tweets(os.path.join('datos', 'pin_tweets.csv'))
