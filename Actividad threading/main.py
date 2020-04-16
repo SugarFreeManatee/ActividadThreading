4f16502b49049355ebeddf2fa87d776bf1b7581f
Matias Duhalde <matias.duhalde@uc.cl>
2020-04-16 19:04:47 -0400

    Modularize tweet retrieval

--- Actividad threading/main.py
+++ Actividad threading/main.py
@@ -1,64 +1,28 @@
-from threading import Lock
-from time import time
+import os
+import sys
+from parametros import bomba_nuclear
+from cargar_tweets import cargar_tweets
 from twitter import Lider_mundial
 from DoomsdayClock import Doomsday_Clock
-import sys
+
 class Simulacion:
 
-    def __init__(self):
-
+    def __init__(self, tweets_pinto, tweets_trumpzini):
         self.doomsday_clock = Doomsday_Clock(0.25, 51)
-        self.kim = Lider_mundial('Dr. Pin Tong-Un', 0.25, self.doomsday_clock)
-        self.trump = Lider_mundial('Trumpzini', 0.25, self.doomsday_clock)
-        
-
+        self.dr_pinto = Lider_mundial('Dr. Pin Tong-Un', tweets_pinto, 0.25, self.doomsday_clock)
+        self.trumpzini = Lider_mundial('Trumpzini',tweets_trumpzini, 0.25, self.doomsday_clock)
 
     def comenzar(self):
         self.doomsday_clock.start()
-        self.kim.start()
-        self.trump.start()
+        self.dr_pinto.start()
+        self.trumpzini.start()
         self.doomsday_clock.join()
-        self.kim.kill = True
-        self.trump.kill = True
-        print('''
-
-
-
-                                               
-
-                                 ________________
-                          ____/ (  (    )   )  \___
-                         /( (  (  )   _    ))  )   )\
-                       ((     (   )(    )  )   (   )  )
-                     ((/  ( _(   )   (   _) ) (  () )  )
-                    ( (  ( (_)   ((    (   )  .((_ ) .  )_
-                   ( (  )    (      (  )    )   ) . ) (   )
-                  (  (   (  (   ) (  _  ( _) ).  ) . ) ) ( )
-                  ( (  (   ) (  )   (  ))     ) _)(   )  )  )
-                 ( (  ( \ ) (    (_  ( ) ( )  )   ) )  )) ( )
-                  (  (   (  (   (_ ( ) ( _    )  ) (  )  )   )
-                 ( (  ( (  (  )     (_  )  ) )  _)   ) _( ( )
-                  ((  (   )(    (     _    )   _) _(_ (  (_ )
-                   (_((__(_(__(( ( ( |  ) ) ) )_))__))_)___)
-                   ((__)        \\||lll|l||///          \_))
-                            (   /(/ (  )  ) )\   )
-                          (    ( ( ( | | ) ) )\   )
-                           (   /(| / ( )) ) ) )) )
-                         (     ( ((((_(|)_)))))     )
-                          (      ||\(|(|)|/||     )
-                        (        |(||(||)||||        )
-                          (     //|/l|||)|\\ \     )
-                        (/ / //  /|//||||\\  \ \  \ _)
-    -------------------------------------------------------------------------------
-
-
-            ''')
+        print(bomba_nuclear)
         sys.exit()
 
+if __name__ == "__main__":
+    tweets_pinto = cargar_tweets(os.path.join('datos', 'pin_tweets.csv'))
+    tweets_trumpzini = cargar_tweets(os.path.join('datos', 'trumpzini_tweets.csv'))
 
-if __name__ == '__main__':
-
-    simulacion = Simulacion()
+    simulacion = Simulacion(tweets_pinto, tweets_trumpzini)
     simulacion.comenzar()
-    
-    
--- Actividad threading/twitter.py
+++ Actividad threading/twitter.py
@@ -1,39 +1,40 @@
 from threading import Thread, Lock
 from time import sleep
-from tweets import tweet_dict
 import random
 
 class Lider_mundial(Thread):
 
     twitear_lock = Lock()
 
-
-    def __init__(self, nombre, enojo, reloj):
+    def __init__(self, nombre, tweets, enojo, reloj):
         super(Lider_mundial,self).__init__()
         self.daemon = True
         self.nombre = nombre
         self._enojo = enojo
         self.reloj = reloj
-        self.tweets = tweet_dict[self.nombre]
+        self.tweets = tweets
         self.kill = False
         random.shuffle(self.tweets)
+
     @property
     def enojo(self):
         return self._enojo
+
     @enojo.setter
     def enojo(self,value):
         if value < 1:
             return 1
         self._enojo = value
-    
+
     def run(self):
         while self.kill == False:
             sleep(1/self.enojo)
             self.twitear()
+
     def twitear(self):
         with self.twitear_lock:
-                tweet = self.tweets.pop()
-                print(f'{self.nombre}: {tweet[1]}')
-                self.reloj.acelerar(self.nombre, int(tweet[0]))
-                self.enojo += int(tweet[0])
-                sleep(1)
+            tweet = self.tweets.pop()
+            print(f'{self.nombre}: {tweet[1]}')
+            self.reloj.acelerar(self.nombre, int(tweet[0]))
+            self.enojo += int(tweet[0])
+            sleep(1)
